79b823c2982ce027ef67bbd50cc015c341adea21
Jean Everton <jeanever39@gmail.com>
2021-06-10 11:07:37 -0300

    Removendo item da lista com SPLICE

---
+++ 7-removendoElemento.js
@@ -0,0 +1,33 @@
+console.log(`Trabalhando com Lista Removendo Elemento`);
+
+// comentar varias linha comando ctrl + k + c 
+// tirar o comentario de varias linhas ctrl + k + u
+// const salvador = ;
+// const SaoPaulo = ;
+// const rioDeJaneiro = ;
+
+//lista Array 
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+)
+
+listaDeDestinos.push(`Curitiba`) // adicionando item na lista com o push
+listaDeDestinos.push(`Paiçandu`) // adicionando item na lista com o push
+listaDeDestinos.push(`Maringá`) // adicionando item na lista com o push
+listaDeDestinos.push(`Sarandi`) // adicionando item na lista com o push
+listaDeDestinos.push(`Marialva`) // adicionando item na lista com o push
+
+console.log("Destinos possíveis: ");
+// console.log(salvador, SaoPaulo, rioDeJaneiro);
+
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); // splice, comando para removendo um item da lista
+console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1], listaDeDestinos[0]); // mostrando somente um elemtento da lista array
+
+listaDeDestinos.pop(); // removendo o ultimo elemtendo da lista 
+console.log(listaDeDestinos);
