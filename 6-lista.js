dadbef301f02e8096b61f96dd4d81391bc977cdb
Jean Everton <jeanever39@gmail.com>
2021-06-10 10:51:58 -0300

    Adicionando lista Array, e adicionando elemento com push

---
+++ 6-lista.js
@@ -0,0 +1,21 @@
+console.log(`Trabalhando com Lista`);
+
+// comentar varias linha comando ctrl + k + c 
+// tirar o comentario de varias linhas ctrl + k + u
+// const salvador = ;
+// const SaoPaulo = ;
+// const rioDeJaneiro = ;
+
+//lista Array 
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+)
+
+listaDeDestinos.push(`Curitiba`) // adicionando item na lista com o push
+
+console.log("Destinos possíveis: ");
+// console.log(salvador, SaoPaulo, rioDeJaneiro);
+
+console.log(listaDeDestinos);
